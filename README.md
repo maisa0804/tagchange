3853b7cecf7f9c38946bcbcf83531a83c5834549
maisa0804 <breitmeer.0804@gmail.com>
2024-10-09 17:43:46 +0900

    Added READ ME and installed libraries

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 ## TagChange
+This is a web application which allow new comers in Japan to make your company to do what you wanna do.
+
+## For who？
+
+- You can find local fellows who are willing to help or join your company.
+
+- No whatter how small you would like to do, that can be found by local.
+
+- Easy to create your account.
+
+- You will offer language or culture exchange.
+
+- 言語、異文化交換をしながら、日本にいる外国の方と一緒に日本を再発見(?)。
+
+- ボランティアやアルバイトをしてみたいけど、ハードルを低く、カジュアルにコミュニティに参加したい。
+
+# Features
+
+For Newcomers
+- Sign up / Login
+- Create event
+- Create profile
+- Chat with members
+
+For Locals
+- Sign up / Login
+- Create 
+- Create profile
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import * as React from "react";
+import { NextUIProvider } from "@nextui-org/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,11 +27,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <NextUIProvider>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </NextUIProvider>
     </html>
   );
 }
